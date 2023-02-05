34bc7f31af197662f89d3fdc3ddd34a3803686cf
Val-Kon-211 <95030676+Val-Kon-211@users.noreply.github.com>
2023-02-05 21:34:20 +0400

    Update first.py

--- Task1/first.py
+++ Task1/first.py
@@ -4,7 +4,5 @@ from dagster import asset
 @asset
 def df_site() -> None: 
     df = pd.read_csv("original.csv")
-    print(df)
     df['domain_of_url'] = df['url'].map(lambda x: str(x).partition("://")[2].partition("/")[0])
-    print(df)
     df.to_csv('norm.csv')
